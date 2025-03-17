ef29d410e3dfe75059f91325ad43c969c28b286c
yskim6772 <yskim6772@gmail.com>
2025-03-17 21:59:07 +0900

    #16 [chore] : 일기 전문 예시 수정

--- app/src/main/java/com/konkuk/strhat/feature/diary/DiaryAIFeedbackViewModel.kt
+++ app/src/main/java/com/konkuk/strhat/feature/diary/DiaryAIFeedbackViewModel.kt
@@ -28,6 +28,6 @@ class DiaryAIFeedbackViewModel @Inject constructor() : ViewModel() {
         _diaryAIFeedbackPositiveKeywords.value = listOf("희망", "노력", "행복")
         _diaryAIFeedbackNegativeKeywords.value = listOf("스트레스", "조급함", "지침")
         _diaryAIFeedbackRecommendation. value = "시험 스트레스를 조금이나마 덜어줄 수 있는 방법으로, 독서와 음악을 조합해보는 건 어떨까요? 좋아하는 음악을 들으며 독서를 즐기면서 마음을 편하게 해보세요. 그러면 조금 더 나아질 거에요."
-        _totalDiary.value = "3월 17일 일기 전문 예시입니다."
+        _totalDiary.value = "3월 17일 일기 전문 예시입니다. 시험 기간에 관한 일기입니다."
     }
 }
