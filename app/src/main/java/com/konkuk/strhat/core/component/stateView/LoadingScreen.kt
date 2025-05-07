88c0e30fe516629c11e095f009488242ee81535b
MinseoSONG <mins041600@naver.com>
2025-05-07 11:33:02 +0900

    #25 [feat] : 로딩뷰 구현

---
+++ app/src/main/java/com/konkuk/strhat/core/component/stateView/LoadingScreen.kt
@@ -0,0 +1,35 @@
+package com.konkuk.strhat.core.component.stateView
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.konkuk.strhat.ui.theme.StrHatTheme
+import com.konkuk.strhat.ui.theme.StrHatTheme.colors
+
+@Composable
+fun LoadingScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .background(color = colors.Gray200)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        LoadingViewAnimation()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ShowLoadingScreen() {
+    StrHatTheme {
+        LoadingScreen()
+    }
+}
