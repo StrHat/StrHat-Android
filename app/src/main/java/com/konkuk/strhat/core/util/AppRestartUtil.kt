4e9473e6c5f5814d29c86b3e2d702db632b9dd18
MinseoSONG <mins041600@naver.com>
2025-05-13 20:41:39 +0900

    #28 [feat] : 로그아웃 로직 수정

--- app/src/main/java/com/konkuk/strhat/core/util/AppRestartUtil.kt
+++ app/src/main/java/com/konkuk/strhat/core/util/AppRestartUtil.kt
@@ -1,28 +1,16 @@
 package com.konkuk.strhat.core.util
 
-import android.app.AlarmManager
-import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import com.konkuk.strhat.feature.main.MainActivity
-import kotlin.system.exitProcess
 
 object AppRestartUtil {
     fun restartApp(context: Context) {
         val intent = Intent(context, MainActivity::class.java).apply {
-            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or
+                    Intent.FLAG_ACTIVITY_CLEAR_TASK or
+                    Intent.FLAG_ACTIVITY_CLEAR_TOP
         }
-
-        val pendingIntent = PendingIntent.getActivity(
-            context,
-            0,
-            intent,
-            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_CANCEL_CURRENT
-        )
-
-        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        alarmManager.set(AlarmManager.RTC, System.currentTimeMillis() + 100, pendingIntent)
-
-        exitProcess(0)
+        context.startActivity(intent)
     }
 }
