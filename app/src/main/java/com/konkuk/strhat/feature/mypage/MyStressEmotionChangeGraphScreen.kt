64c7e171d58f8341be724d2685eafb4034419812
yskim6772 <yskim6772@gmail.com>
2025-05-18 00:50:19 +0900

    #33 [chore] : 주간 그래프 popBackStack 시 상태 유지

--- app/src/main/java/com/konkuk/strhat/feature/mypage/MyStressEmotionChangeGraphScreen.kt
+++ app/src/main/java/com/konkuk/strhat/feature/mypage/MyStressEmotionChangeGraphScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -50,7 +51,7 @@ fun MyStressEmotionChangeGraphRoute(
     navigateToMyPageAIFeedback: (String) -> Unit,
     viewModel: MyStressGraphViewModel = hiltViewModel()
 ) {
-    var weekOffset by remember { mutableIntStateOf(0) }
+    var weekOffset by rememberSaveable { mutableIntStateOf(0) }
     val initialDate = remember(date) { LocalDate.parse(date) }
 
     LaunchedEffect(weekOffset) {
