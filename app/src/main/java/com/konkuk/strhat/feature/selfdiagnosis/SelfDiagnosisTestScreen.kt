e42da868b72b7619462a00acefcee5f23accebae
yskim6772 <yskim6772@gmail.com>
2025-05-19 02:00:09 +0900

    #36 [feat] : 자가진단 테스트 뷰 문구들 분기처리

--- app/src/main/java/com/konkuk/strhat/feature/selfdiagnosis/SelfDiagnosisTestScreen.kt
+++ app/src/main/java/com/konkuk/strhat/feature/selfdiagnosis/SelfDiagnosisTestScreen.kt
@@ -27,9 +27,14 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.konkuk.strhat.R
 import com.konkuk.strhat.core.component.button.StrHatButton
+import com.konkuk.strhat.core.util.KeyStorage.SELECTION_COUNT_4
+import com.konkuk.strhat.core.util.KeyStorage.SELECTION_COUNT_5
+import com.konkuk.strhat.core.util.KeyStorage.SELECTION_COUNT_NONE
 import com.konkuk.strhat.domain.entity.SelfDiagnosisItem
 import com.konkuk.strhat.domain.entity.SelfDiagnosisModel
-import com.konkuk.strhat.domain.type.SelfDiagnosisTestType
+import com.konkuk.strhat.domain.type.SelfDiagnosisTestType.PHQ9
+import com.konkuk.strhat.domain.type.SelfDiagnosisTestType.PSS
+import com.konkuk.strhat.domain.type.SelfDiagnosisTestType.SRI
 import com.konkuk.strhat.ui.theme.StrHatTheme
 import com.konkuk.strhat.ui.theme.StrHatTheme.colors
 import com.konkuk.strhat.ui.theme.StrHatTheme.typography
@@ -82,10 +87,23 @@ fun SelfDiagnosisTestScreen(
     val selections = remember { mutableStateMapOf<Int, Int>() }
 
     val selfDiagnosisTestScreenTitle = when (type) {
-        SelfDiagnosisTestType.PSS.testType -> stringResource(R.string.self_diagnosis_test_PSS_title)
-        SelfDiagnosisTestType.SRI.testType -> stringResource(R.string.self_diagnosis_test_SRI_title)
-        SelfDiagnosisTestType.PHQ9.testType -> stringResource(R.string.self_diagnosis_test_PHQ_9_title)
+        PSS.testType -> stringResource(R.string.self_diagnosis_test_PSS_title)
+        SRI.testType -> stringResource(R.string.self_diagnosis_test_SRI_title)
+        PHQ9.testType -> stringResource(R.string.self_diagnosis_test_PHQ_9_title)
         else -> stringResource(R.string.self_diagnosis_test_default_title)
+    }
+
+    val selfDiagnosisTestScreenDescription = when (type) {
+        PSS.testType -> stringResource(R.string.self_diagnosis_test_PSS_selection_description)
+        SRI.testType -> stringResource(R.string.self_diagnosis_test_SRI_selection_description)
+        PHQ9.testType -> stringResource(R.string.self_diagnosis_test_PHQ_9_selection_description)
+        else -> stringResource(R.string.blank)
+    }
+
+    val selectionCount = when (type) {
+        PSS.testType, SRI.testType -> SELECTION_COUNT_5
+        PHQ9.testType -> SELECTION_COUNT_4
+        else -> SELECTION_COUNT_NONE
     }
 
     Column(
@@ -99,7 +117,7 @@ fun SelfDiagnosisTestScreen(
         )
         Spacer(modifier = Modifier.height(16.dp))
         Text(
-            text = stringResource(R.string.self_diagnosis_test_selection_description),
+            text = selfDiagnosisTestScreenDescription,
             style = typography.body3_m_14,
             color = colors.Gray400
         )
@@ -123,7 +141,7 @@ fun SelfDiagnosisTestScreen(
                         horizontalArrangement = Arrangement.spacedBy(12.dp),
                         verticalAlignment = Alignment.CenterVertically
                     ) {
-                        (1..5).forEach { score ->
+                        (1..selectionCount).forEach { score ->
                             Row(
                                 verticalAlignment = Alignment.CenterVertically
                             ) {
@@ -138,7 +156,13 @@ fun SelfDiagnosisTestScreen(
                                         unselectedColor = colors.Gray500
                                     )
                                 )
-                                Text(text = score.toString())
+                                Text(
+                                    text =
+                                        if (type == SRI.testType)
+                                            score.toString()
+                                        else
+                                            (score - 1).toString()
+                                )
                             }
                         }
                     }
